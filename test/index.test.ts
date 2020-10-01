5ba25bc886b5621f35acc3e15f97987b40befc54
Pavol Hejny <me@pavolhejny.com>
2020-10-01 13:38:59 +0200

    Linter

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,21 +10,21 @@ test('PDFMK', () => {
 
     expect(async () => {
         const url = maker({
-            url: `https://pdfmk.com/api`,
             token: `test-test-test-test`,
+            url: `https://pdfmk.com/api`,
         })
             .open({
                 url: `https://www.pavolhejny.com/`,
             })
             .load({ waitUntil: 'domcontentloaded' })
             .toPdf({
-                scale: 1,
                 margin: {
                     top: '1cm',
                 },
-            })
-            .getUnsecurePublicUrlSync()
-            .toString();
+                scale: 1,
+            });
+        // .getUnsecurePublicUrlSync()
+        // .toString();
 
         console.log(url);
     }).not.toThrowError();
